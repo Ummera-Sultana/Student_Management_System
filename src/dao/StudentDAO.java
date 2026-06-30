13fe68f6bd6a7f55b45644266a64ca97c7f8609f
= <=>
2026-07-01 00:09:01 +0530

    Complete Student Management System CRUD application using Java JDBC and MySQL

--- src/dao/StudentDAO.java
+++ src/dao/StudentDAO.java
@@ -57,4 +57,85 @@ public class StudentDAO {
         }
    return lis;
     }
+    public Student searchStudentById(int ID) {
+        String Query = "SELECT * FROM students WHERE id=?";
+        try (Connection con = DBConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement(Query)) {
+           ps.setInt(1,ID);
+            ResultSet rs=ps.executeQuery();
+           if(rs.next()){
+               int ids=rs.getInt("id");
+               String names= rs.getString("name");
+               int age=rs.getInt("age");
+               String course=rs.getString("course");
+               String email=rs.getString("email");
+               Student stu=new Student(ids,names,age,course,email);
+               return stu;
+
+           }
+
+        } catch (SQLException ex) {
+            System.out.println("search Exception Occured");
+            throw new RuntimeException(ex);
+        }
+        return null;
+    }
+    public List<Student> searchStudentByName(String name){
+        List<Student> Students=new ArrayList<>();
+        final String query="SELECT * FROM students WHERE name =?";
+        try(Connection con= DBConnection.getConnection();
+        PreparedStatement st=con.prepareStatement(query)){
+
+            st.setString(1,name);
+            ResultSet rs= st.executeQuery();
+            while(rs.next()){
+                int ids=rs.getInt("id");
+                String names= rs.getString("name");
+                int age=rs.getInt("age");
+                String course=rs.getString("course");
+                String email=rs.getString("email");
+                Student stu=new Student(ids,names,age,course,email);
+                Students.add(stu);
+
+            }
+
+        } catch (SQLException e) {
+            System.out.println("SearchByName Exception");
+            throw new RuntimeException(e);
+        }
+        return Students;
+    }
+    public boolean updateStudents(Student student){
+        final String query="UPDATE students SET name=?,age=?,course=?,email=? WHERE id=? ";
+        try(Connection conn=DBConnection.getConnection();
+        PreparedStatement ps=conn.prepareStatement(query)){
+            ps.setString(1,student.getName());
+            ps.setInt(2,student.getAge());
+            ps.setString(3,student.getCourse());
+            ps.setString(4,student.getEmail());
+            ps.setInt(5,student.getId());
+            int RowsAffected=ps.executeUpdate();
+            return RowsAffected>0;
+
+        }catch(SQLException e){
+            System.out.println("Update Exception Occured");
+            e.printStackTrace();
+        }
+        return false;
+    }
+    public boolean deleteStudentsById(int id){
+        final String query="DELETE FROM students WHERE id=?";
+        try(Connection con=DBConnection.getConnection();
+        PreparedStatement ps=con.prepareStatement(query)){
+            ps.setInt(1,id);
+            int rowsAffected=ps.executeUpdate();
+            return rowsAffected>0;
+
+        }catch(SQLException e){
+            System.out.println("Delete Exception Occuured");
+            e.printStackTrace();
+        }
+
+        return  false;
+    }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,14 @@
 import model.Student;
  import dao.StudentDAO;
+
 import java.util.Scanner;
 void main() {
     Scanner ip=new Scanner(System.in);
 while(true) {
-    System.out.println("-----Student Management System-----");
-    System.out.println("1.Add Student\n 2.View Students \n 3.Exit");
+    System.out.println("========================================");
+    System.out.println("      Student Management System");
+    System.out.println("=========================================");
+    System.out.println(" 1.Add Student\n 2.View Students \n3.Search Student \n 4.Update Students Details\n5.Delete Student\n 6.Exit");
     System.out.print("Enter your Choice:");
     int option=ip.nextInt();
     ip.nextLine();
@@ -43,8 +46,95 @@ while(true) {
             }
             break;
         case 3:
+            System.out.println("Select the way to search\n 1.Search Student By Id \n2.Search student By Name");
+            int ops=ip.nextInt();
+            ip.nextLine();
+            switch(ops) {
+                case 1:
+                    System.out.print("Enter ID of Student:");
+                    int id = ip.nextInt();
+                    ip.nextLine();
+
+                    Student sts = daos.searchStudentById(id);
+                    if (sts != null) {
+                        System.out.println("---------------------");
+                        System.out.println("ID: " + sts.getId());
+                        System.out.println("Name: " + sts.getName());
+                        System.out.println("Age: " + sts.getAge());
+                        System.out.println("Course: " + sts.getCourse());
+                        System.out.println("Email: " + sts.getEmail());
+                        System.out.println("----------------------");
+                    } else {
+                        System.out.println("Student not found");
+                    }
+                    break;
+                case 2:
+                    System.out.print("Enter the Name of Student:");
+                    String nam = ip.nextLine();
+
+                    List<Student> sts2 = daos.searchStudentByName(nam);
+                    if (sts2.isEmpty()) {
+                        System.out.println("No Students Found");
+                    } else {
+                        for (Student student : sts2) {
+                            System.out.println("---------------------");
+                            System.out.println("ID: " + student.getId());
+                            System.out.println("Name: " + student.getName());
+                            System.out.println("Age: " + student.getAge());
+                            System.out.println("Course: " + student.getCourse());
+                            System.out.println("Email: " + student.getEmail());
+                            System.out.println("----------------------");
+                        }
+                    }
+                        break;
+                        case 3:
+                            return;
+
+                default:
+                    System.out.println("Invalid choice! Please try again.");
+
+                    }
+                    break;
+
+
+
+        case 4:
+            System.out.println("Enter ID");
+            int ids=ip.nextInt();
+            ip.nextLine();
+            System.out.print("Enter name:");
+            String names = ip.nextLine();
+            System.out.print("\nEnter Age:");
+            int ages = ip.nextInt();
+            ip.nextLine();
+            System.out.print("\nEnter Course:");
+            String courses = ip.nextLine();
+            System.out.print("\n Enter email:");
+            String emails = ip.nextLine();
+            Student st1=new Student(ids,names,ages,courses,emails);
+
+            if(daos.updateStudents(st1)){
+                System.out.println("Student Updated Succesfully");
+            }else{
+                System.out.println("Student ID not found!");
+            }
+            break;
+        case 5:
+            System.out.println("Enter ID");
+            int ID=ip.nextInt();
+            if(daos.deleteStudentsById(ID)){
+                System.out.println("Student Deleted Succesfully");
+            }else{
+                System.out.println("Student Not Found!");
+            }
+            break;
+
+
+        case 6:
             System.out.println("THANK YOU ");
             return;
+        default:
+            System.out.println("Invalid choice! Please try again.");
     }
 }
 
